bef41a0ea0f1c8ffeef8c9823c57800dc4f79925
Satoshi T <sulp91_fy@yahoo.co.jp>
2021-09-05 01:06:11 +0900

    Modify command logic

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 require('dotenv').config();
 const { App } = require('@slack/bolt');
-const top = require('./top');
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+// コマンド
+app.command('/heroku-sample', async ({ack, say, command, logger}) => {
+    logger.info(`${command.command} start`)
+    await ack();
+    const message = `こんにちは、<@${command.user_name}> from heroku`;
+    await say({
+        blocks: [
+            {
+                type: 'section',
+                text: {
+                    type: 'mrkdwn',
+                    text: message
+                },
+                accessory: {
+                    type: 'button',
+                    text: {
+                        type: 'plain_text',
+                        text: 'クリック！'
+                    },
+                    action_id: 'button_click'
+                }
+            }
+        ],
+        text: message
+    });
+});
+
+// アクション
+app.action('button_click', async ({ack, body, say, logger, action}) => {
+    logger.info(`${action.name} start`)
+    await ack();
+    await say(`${body.user.name}さんがボタンをクリックしました`);
 });
 
 // アプリ起動
